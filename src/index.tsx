1b64bb219af292e16fd74d772a926badaa53aff6
tomfang678 <tomfang@126.com>
2020-06-12 16:32:17 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+// @ts-ignore
+import React from 'react';
+// @ts-ignore
+import ReactDOM from 'react-dom';
+import {PolestarApp} from "@polestar/component-warehouse-react";
+import * as serviceWorker from './serviceWorker';
+import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <PolestarApp>
+            <App/>
+        </PolestarApp>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+serviceWorker.unregister();
